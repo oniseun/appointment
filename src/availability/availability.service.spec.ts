eaeb5758edf05d8fdbcea0e3ccc0a117c3a86ec8
Victor Oni <seunoni34@gmail.com>
2022-04-01 19:17:56 +0200

    add test for availability service

--- src/availability/availability.service.spec.ts
+++ src/availability/availability.service.spec.ts
@@ -1,187 +1,196 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AvailabilityService } from './availability.service';
+import { IAvailability } from './interfaces/availability.interface';
+import { DoctorsService } from '../doctors/doctors.service';
 import { NotFoundException } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
-import { IAvailability } from './interfaces/availability.interface';
+import { IDoctor } from '../doctors/interfaces/doctor.interface';
 import { Model } from 'mongoose';
+import { CreateAvailabilityInput, FindAvailabilityInput } from './availability.model';
+import { ConfigService } from '@nestjs/config';
+import { DateTime } from 'luxon';
 
-// const mockCustomer: any = {
-//   firstName: 'firstName #1',
-//   lastName: 'lastName #1',
-//   email: 'test@example.it',
-//   phone: '1234567890',
-//   address: 'address #1',
-//   description: 'description #1',
-//   organizations: 'organization #1',
-// };
+const mockDoctor: any = {
+    _id: "623a4be15eec415b89ed269",
+    fullName: 'Fola Scholz',
+    age: 40,
+    specialization: 'Dentist'
+};
+const weekDay = (new Date()).getDay();
+let testDate = DateTime.now().plus({days: [0, 5, 6].includes(weekDay) ? 4 : 1 }).toISODate()
 
-// const mockCustomerUpdate: any = {
-//   _id: 'anyid',
-//   firstName: 'firstName update',
-//   lastName: 'lastName update',
-//   email: 'test@example.it',
-//   phone: '1234567890',
-//   address: 'address update',
-//   description: 'description update',
-//   organizations: 'organization update',
-// };
+const findAvailabilityInput: FindAvailabilityInput = {
+    doctorId: "623a4be15eec415b89ed269",
+    fromDate: testDate,
+    toDate: testDate,
+    duration: 30
+  };
 
-// const customersArray = [
-//   {
-//     _id: 'anyid',
-//     firstName: 'firstName #1',
-//     lastName: 'lastName #1',
-//     email: 'test@example.it',
-//     phone: '1234567890',
-//     address: 'address #1',
-//     description: 'description #1',
-//     organizations: 'organization #1',
-//   },
-//   {
-//     _id: 'anyid',
-//     firstName: 'firstName #2',
-//     lastName: 'lastName #2',
-//     email: 'test@example.it',
-//     phone: '1234567890',
-//     address: 'address #2',
-//     description: 'description #2',
-//     organizations: 'organization #2',
-//   },
-// ];
+  const createAvailabilityInput: CreateAvailabilityInput = {
+    doctorId: "623a4be15eec415b89ed269",
+    date: testDate,
+    timeslots: [{
+        fromTime: '09:00',
+        toTime: '10:00'
+    }],
+  };
 
-// const createCustomerDto: CreateDoctorDto = {
-//   firstName: 'firstName #1',
-//   lastName: 'lastName #1',
-//   email: 'test@example.it',
-//   phone: '1234567890',
-//   address: 'address #1',
-//   description: 'description #1',
-//   organizations: 'organization #1',
-// };
+  const availabilities = [
+      {
+        _id: "id123",
+        doctorId: "623a4be15eec415b89ed269",
+        date: new Date(testDate),
+        fromTime: '',
+        toTime: '',
+        timeslots: JSON.stringify({
+            '09:00' : true,
+            '09:15' : true,
+            '09:30' : true,
+            '09:45' : true,
+            '10:00' : true
+        }),
+        tz: "Europe/Berlin",
+      }
+  ]
 
-// const updateCustomerDto: UpdateDoctorDto = {
-//   firstName: 'firstName update',
-//   lastName: 'lastName update',
-//   email: 'test@example.it',
-//   phone: '1234567890',
-//   address: 'address update',
-//   description: 'description update',
-//   organizations: 'organization update',
-// };
+  const availableTimeSlots = [
+    {
+        doctorId: "623a4be15eec415b89ed269",
+        date: new Date(testDate),
+        fromTime: '09:00',
+        toTime: '09:30'
+    },
+    {
+        doctorId: "623a4be15eec415b89ed269",
+        date: new Date(testDate),
+        fromTime: '09:30',
+        toTime: '10:00'
+    }
+]
 
-// describe('CustomersService', () => {
-//   let service: CustomersService;
-//   let model: Model<ICustomer>;
+describe('AvailabilityService', () => {
+  let service: AvailabilityService;
+  let availabilityModel: Model<IAvailability>;
+  let doctorModel: Model<IDoctor>;
 
-//   const paginationQueryDto: PaginationQueryDto = {
-//     limit: 10,
-//     offset: 1,
-//   };
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [
-//         CustomersService,
-//         {
-//           provide: getModelToken('Customer'),
-//           useValue: {
-//             find: jest.fn().mockReturnValue(customersArray),
-//             findById: jest.fn(),
-//             findByIdAndUpdate: jest.fn(),
-//             findByIdAndRemove: jest.fn(),
-//             new: jest.fn().mockResolvedValue(mockCustomer),
-//             constructor: jest.fn().mockResolvedValue(mockCustomer),
-//             create: jest.fn().mockResolvedValue(createCustomerDto),
-//             findAll: jest.fn(),
-//             findOne: jest.fn(),
-//             update: jest.fn(),
-//             remove: jest.fn(),
-//             exec: jest.fn(),
-//             populate: jest.fn(),
-//             skip: jest.fn(),
-//             offset: jest.fn(),
-//           },
-//         },
-//       ],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AvailabilityService,
+        ConfigService,
+        {
+          provide: getModelToken('Availability'),
+          useValue: {
+            find: jest.fn().mockReturnValue(availabilities),
+            findById: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndRemove: jest.fn(),
+            new: jest.fn().mockResolvedValue(availabilities[0]),
+            constructor: jest.fn().mockResolvedValue(availabilities[0]),
+            create: jest.fn().mockResolvedValue(availabilities[0]),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+            exec: jest.fn(),
+            populate: jest.fn(),
+            skip: jest.fn(),
+            offset: jest.fn(),
+          },
+          
+        },
 
-//     service = module.get<DoctorsService>(DoctorsService);
-//     model = module.get<Model<IDoctor>>(getModelToken('Doctor'));
-//   });
+        {
+          provide: getModelToken('Doctor'),
+          useValue: {
+            find: jest.fn().mockReturnValue([mockDoctor]),
+            findById: jest.fn().mockResolvedValue(mockDoctor),
+            findByIdAndUpdate: jest.fn(),
+            findByIdAndRemove: jest.fn(),
+            new: jest.fn().mockResolvedValue(mockDoctor),
+            constructor: jest.fn().mockResolvedValue(mockDoctor),
+            create: jest.fn().mockResolvedValue(mockDoctor),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+            exec: jest.fn(),
+            populate: jest.fn(),
+            skip: jest.fn(),
+            offset: jest.fn(),
+          },
+          
+        },
+      ],
+    }).compile();
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
+    service = module.get<AvailabilityService>(AvailabilityService);
+    availabilityModel = module.get<Model<IAvailability>>(getModelToken('Availability'));
+    doctorModel = module.get<Model<IDoctor>>(getModelToken('Doctor'));
+  });
 
-//   describe('findAll()', () => {
-//     it('should return all customers', async () => {
-//       jest.spyOn(model, 'find').mockReturnValue({
-//         exec: jest.fn().mockResolvedValueOnce(customersArray),
-//         skip: jest.fn().mockReturnThis(),
-//         limit: jest.fn().mockReturnThis(),
-//         populate: jest.fn().mockReturnThis(),
-//       } as any);
-//       const customers = await service.findAll(paginationQueryDto);
-//       expect(customers).toEqual(customersArray);
-//     });
-//   });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
 
-//   describe('findOne()', () => {
-//     it('should return one customer', async () => {
-//       const findSpy = jest.spyOn(model, 'findById').mockReturnValueOnce({
-//         exec: jest.fn().mockResolvedValueOnce(mockCustomer),
-//         populate: jest.fn().mockReturnThis(),
-//       } as any);
-//       const response = await service.findOne('anyid');
-//       expect(findSpy).toHaveBeenCalledWith({ _id: 'anyid' });
-//       expect(response).toEqual(mockCustomer);
-//     });
+  describe('findAll()', () => {
+    it('should return availabilties from 09:00 - 10:00', async () => {
+   jest.spyOn(availabilityModel, 'find').mockReturnValue({
+        exec: jest.fn().mockResolvedValueOnce(availabilities),
+        skip: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        populate: jest.fn().mockReturnThis(),
+      } as any); 
+    
+    jest.spyOn(doctorModel, 'findById').mockReturnValue({
+        exec: jest.fn().mockResolvedValueOnce(mockDoctor),
+        skip: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        populate: jest.fn().mockReturnThis(),
+    } as any); 
 
-//     it('should throw if find one customer throws', async () => {
-//       jest.spyOn(model, 'findById').mockReturnValueOnce({
-//         exec: jest.fn(() => null),
-//         populate: jest.fn().mockReturnThis(),
-//       } as any);
-//       await expect(service.findOne('anyid')).rejects.toThrow(
-//         new NotFoundException('Customer #anyid not found'),
-//       );
-//     });
-//   });
+      const available_slots = await service.findAll(findAvailabilityInput);
+      expect(available_slots).toStrictEqual(availableTimeSlots);
+    });
+  });
 
-//   describe('create()', () => {
-//     it('should insert a new organization', async () => {
-//       jest.spyOn(model, 'create').mockImplementationOnce(() =>
-//         Promise.resolve({
-//           _id: 'a id',
-//           firstName: 'firstName #1',
-//           lastName: 'lastName #1',
-//           email: 'test@example.it',
-//           phone: '1234567890',
-//           address: 'address #1',
-//           description: 'description #1',
-//           organizations: 'organization #1',
-//         }),
-//       );
-//       const newCustomer = await service.create({
-//         firstName: 'firstName #1',
-//         lastName: 'lastName #1',
-//         email: 'test@example.it',
-//         phone: '1234567890',
-//         address: 'address #1',
-//         description: 'description #1',
-//         organizations: 'organization #1',
-//       });
-//       expect(newCustomer).toEqual({
-//         _id: 'a id',
-//         firstName: 'firstName #1',
-//         lastName: 'lastName #1',
-//         email: 'test@example.it',
-//         phone: '1234567890',
-//         address: 'address #1',
-//         description: 'description #1',
-//         organizations: 'organization #1',
-//       });
-//     });
-//   });
+  describe('create()', () => {
+    it('should create availability for a doctor given an input', async () => {
+        jest.spyOn(availabilityModel, 'findOne').mockReturnValue({
+            exec: jest.fn().mockResolvedValueOnce(undefined),
+            skip: jest.fn().mockReturnThis(),
+            limit: jest.fn().mockReturnThis(),
+            populate: jest.fn().mockReturnThis(),
+        } as any
+      );
+      jest.spyOn(availabilityModel, 'create').mockImplementationOnce(() =>
+        Promise.resolve({
+            _id: "id123",
+            doctorId: "623a4be15eec415b89ed269",
+            date: new Date(testDate),
+            fromTime: '',
+            toTime: '',
+            timeslots: JSON.stringify({
+                '09:00' : true,
+                '09:15' : true,
+                '09:30' : true,
+                '09:45' : true,
+                '10:00' : true
+            }),
+            tz: "Europe/Berlin",
+          }),
+      );
+      jest.spyOn(doctorModel, 'findById').mockReturnValue({
+        exec: jest.fn().mockResolvedValueOnce(mockDoctor),
+        skip: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        populate: jest.fn().mockReturnThis(),
+    } as any); 
 
-// });
+      const newAvailability = await service.create(createAvailabilityInput);
+      expect(newAvailability).toStrictEqual(availabilities[0]);
+    });
+  });
+
+});
